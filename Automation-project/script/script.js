8df3b3f607417f51968ccd2f2d1301d8b4f20804
Vikki <vikkikuswaha9@gmail.com>
2023-10-24 01:27:42 +0530

    add some code

--- Automation-project/script/script.js
+++ Automation-project/script/script.js
@@ -1,6 +1,7 @@
 const singup_Button = document.getElementById("singUp_Button");
 const login_btn = document.getElementById("login-btn");
 const loginPage = document.querySelector(".side-login-page");
+const  registerLandBtn = document.querySelector(".land_Btn")
 
 
  const submitForm = (event)=>{
@@ -12,7 +13,10 @@ const login = () => {
     loginPage.classList.toggle("side-login-page-block")
 }
 
-
+const register = () => {
+    document.querySelector(".register-land").classList.toggle("register-land-none")
+}
 
 singup_Button.addEventListener("click",submitForm)
 login_btn.addEventListener("click",login)
+registerLandBtn.addEventListener("click",register)
